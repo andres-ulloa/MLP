7a953bc15743638c99c20cd2b9b72382e3d4f911
andres <andresulloa97@gmail.com>
2019-03-02 10:34:32 -0600

    classification routine

--- ANN.py
+++ ANN.py
@@ -2,12 +2,33 @@
 import numpy as np
 import pandas as pd
 
+label_position = 28 * 28
+
 
 def compute_cost_function():
     pass
 
 def compute_activation_function(activatiom_func):
     pass
+
+def classify(dataset, model):
+
+    global label_position
+    labels = list()
+    examples_counter = 0
+    likelihood = list()
+    
+    for example in dataset:
+
+        examples_counter +=1 
+        likelihood = list()
+        
+        #print('Likelihood of belonging to each possible class = \n\n', likelihood)
+        likelihood.sort(key = lambda tup: tup[0])
+        label = (examples_counter, likelihood[len(likelihood) - 1][1], example[label_position])
+        labels.append(label)
+    
+    return labels
 
 def trim_dataset(dataset, train_set_size, test_set_size):
     
@@ -17,10 +38,10 @@ def trim_dataset(dataset, train_set_size, test_set_size):
     for i in range(0, train_set_size + test_set_size):
         if i < train_set_size:
             training_set.append(dataset[i])
-        if i > train_set_size:
+        if i >= train_set_size:
             test_set.append(dataset[i])
 
-    return train_set_size, test_set
+    return training_set, test_set
 
 
 def compute_confution_matrix(labels):
@@ -49,7 +70,7 @@ def compute_confution_matrix(labels):
     
     confution_matrix = np.array([[true_positives, false_positives],[false_negatives, true_negatives]])
     return confution_matrix
-    
+
 
 def train():
     
@@ -57,9 +78,8 @@ def train():
     input_layer_size = 28 * 28 #there are going to be as much input neurons as there are pixels in each image
     num_classes = 10
     sigmoid_func =  lambda x:  1/(1 + math.exp(- x))
-
     train_test, test_set = trim_dataset(dataset, 900, 100)
-
+    print(len(train_test), len(test_set))
 
 if __name__ == '__main__':
     train()
